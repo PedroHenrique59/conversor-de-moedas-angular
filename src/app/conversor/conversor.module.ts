9eb04c458d8be249217c9abdf1b477f7713e8db2
Pedro Henrique <pedro.henrique59@outlook.com>
2023-01-09 09:46:53 -0300

    criando directive numero e atualizando html.

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -4,14 +4,18 @@ import { ConversorComponent } from './components/conversor.component';
 import { MoedaService } from './services/moeda.service';
 import { ConversorService } from './services/conversor.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NumeroDirective } from './directives/numero.directive';
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
+    NumeroDirective
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   exports: [
     ConversorComponent
